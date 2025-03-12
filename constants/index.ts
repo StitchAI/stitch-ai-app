7b87003f3e6ac038cb26cce1898424ba6d92bd32
0xyz148 <chaaaamni2@gmail.com>
2025-03-13 02:09:40 +0900

    fix endpoint

--- constants/index.ts
+++ constants/index.ts
@@ -3,14 +3,6 @@ export const IS_LOCAL = process.env.NEXT_PUBLIC_IS_LOCAL === 'true';
 export const IS_MAINNET = process.env.NEXT_PUBLIC_BLOCKCHAIN_ENV === 'mainnet';
 export const IS_DEVNET = process.env.NEXT_PUBLIC_BLOCKCHAIN_ENV === 'devnet';
 
-export const API_URL = IS_LOCAL
-  ? 'http://localhost:8080'
-  : IS_MAINNET
-    ? ''
-    : IS_DEVNET
-      ? ''
-      : 'http://localhost:8080';
-
 export const SCANNER_URL = IS_MAINNET ? '' : 'https://testnet.monadexplorer.com/';
 
 export const CHAIN_ID = 10143;
