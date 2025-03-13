b475c5ca39adea36bb711e88b2fd5f237504941a
0xyz148 <chaaaamni2@gmail.com>
2025-03-13 11:24:37 +0900

    add external memory buy

--- app/market-place/_components/card/index.tsx
+++ app/market-place/_components/card/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { upperFirst } from 'lodash-es';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
@@ -107,26 +108,93 @@ interface ExternalMemoryCardProps {
   price: number;
   operator?: string;
   operatorLogo?: string;
+
+  listingId?: string;
+  internalId?: string;
 }
 
-export const ExternalMemoryCard = ({ price, operator }: ExternalMemoryCardProps) => {
+export const ExternalMemoryCard = ({
+  price,
+  operator,
+  listingId,
+  internalId,
+}: ExternalMemoryCardProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [buttonText, setButtonText] = useState('Buy');
+  const router = useRouter();
+  const { address } = useAccount();
+  const { open: openSuccessDialog } = useDialog('success');
+
+  const { mutateAsync: approve } = useTxApprove();
+  const { mutateAsync: buyMemory } = useTxMarketBuyMemory();
+  const { mutateAsync: createPurchase } = usePostMarketPurchase(address as string);
+
+  const handleBuy = async () => {
+    if (!address || isLoading || !internalId) return;
+
+    try {
+      setIsLoading(true);
+
+      setButtonText('Approving...');
+      const approveTxReceipt = await approve({
+        amount: price.toString(),
+        spender: CONTRACTS_DEVNET.MEMORY_MARKETPLACE,
+      });
+      if (approveTxReceipt?.status !== 'success') return;
+
+      setButtonText('Buying...');
+      const txReceipt = await buyMemory({
+        memoryId: internalId,
+      });
+      if (txReceipt?.status !== 'success') return;
+
+      setButtonText('Saving...');
+      if (listingId && internalId && txReceipt?.transactionHash) {
+        await createPurchase({
+          buyerId: address,
+          internalListingId: internalId,
+          listingId,
+          price,
+          txHash: txReceipt.transactionHash,
+        });
+      }
+
+      openSuccessDialog({
+        params: {
+          title: 'Memory Purchase Successful!',
+          description: 'This memory has been added to your list.',
+          txHash: txReceipt?.transactionHash || '',
+          buttonText: 'Go to List',
+          onButtonClick: () => {
+            router.push('/memories');
+          },
+        },
+      });
+    } catch (error) {
+      console.error('Memory purchase failed:', error);
+    } finally {
+      setIsLoading(false);
+      setButtonText('Buy');
+    }
+  };
+
   return (
     <div className={style.wrapper}>
       <div className={style.header}>
-        {operator === 'Nansen' && (
+        {operator === 'nansen' && (
           <Image src={'/images/logo-nansen.png'} alt="Nansen" width={48} height={48} />
         )}
-        {operator === 'Chainlink' && (
+        {operator === 'chainlink' && (
           <Image src={'/images/logo-chainlink.png'} alt="Chainlink" width={48} height={48} />
         )}
-        <div className={style.title}>{operator}</div>
+        <div className={style.title}>{upperFirst(operator)}</div>
       </div>
       <div className={style.body}>
         <div className={style.priceWrapper}>
-          <div className={style.price}>{formatNumeric(price, { suffix: ' USDC' })}</div>
+          <div className={style.price}>{formatNumeric(price, { decimal: 4, suffix: ' USDC' })}</div>
           <div className={style.priceLabel}>/month</div>
         </div>
-        <ButtonPrimary text="Buy" size="medium" />
+        <ButtonPrimary text={buttonText} size="medium" onClick={handleBuy} disabled={isLoading} />
       </div>
     </div>
   );
